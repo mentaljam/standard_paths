44fc86518e27442964dcc05caa3e9362bb68c1b9
Petr Tsymbarovich <petr@tsymbarovich.ru>
2017-12-06 23:55:51 +0300

    Add find_executables example

---
+++ examples/find_executables.rs
@@ -0,0 +1,33 @@
+extern crate standard_paths;
+
+use std::env;
+use std::process;
+use standard_paths::*;
+
+fn main() {
+    let args = env::args().skip(1).collect::<Vec<_>>();
+    if args.len() == 0 {
+        println!("\n    Usage: find_executable <path1> [path2 [path3 ...]]\n");
+        process::exit(0);
+    }
+
+    let mut ind: usize = 0;
+    for exe in &args {
+        let len = exe.len();
+        if len < 16 && ind < len {
+            ind = len;
+        }
+    }
+
+    println!("Searching executables:");
+    for exe in args {
+        let fexe = format!("{:>1$}", exe, ind);
+        match StandardLocation::find_executable(exe.clone()) {
+            Some(paths) => println!("{}: {}", fexe, paths.iter()
+                                                        .map(|p| p.to_str().unwrap())
+                                                        .collect::<Vec<_>>()
+                                                        .join(" ")),
+            _ => println!("{}: not found", fexe)
+        }
+    }
+}
